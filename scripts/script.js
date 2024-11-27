4b2f405f6f0a296e7c73d38db34862e46e384300
kimhelina <helinakim02@gmail.com>
2024-11-27 13:29:29 +0100

    header fixed

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// ---------- HAMBURGER ----------------//
 document.addEventListener("DOMContentLoaded", () => {
   const header = document.querySelector("header");
   const hamburgerBtn = document.querySelector("button[aria-label='Open Menu']");
@@ -34,5 +35,57 @@ document.addEventListener("click", (event) => {
     carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });
   } else if (direction === "right") {
     carousel.scrollBy({ left: scrollAmount, behavior: "smooth" });
+  }
+});
+
+// ---------- SEARCH ----------------//
+document.addEventListener("DOMContentLoaded", () => {
+  const searchButton = document.querySelector("button[aria-label='Search']");
+  const closeButton = document.querySelector(
+    "section[aria-hidden] button[aria-label='Close Search']"
+  );
+  const searchPopUp = document.querySelector("section[aria-hidden]");
+  const navButtons = searchPopUp.querySelectorAll(
+    "div[role='navigation'] > button:not(:nth-of-type(2))"
+  );
+  const activeButton = searchPopUp.querySelector(
+    "div[role='navigation'] > button:nth-of-type(2)"
+  );
+
+  const options = [
+    "COMPLIMENTARY FESTIVE DUO",
+    "12 DAY ADVENT CALENDAR",
+    "GINGER BISCUIT",
+    "ORANGE BITTERS",
+    "FIR & ARTEMISIA",
+    "HINOKI & CEDARWOOD",
+    "CHRISTMAS GIFTS",
+    "HOME & CANDLES",
+    "COLOGNES",
+    "GIFTS FOR HER",
+    "GIFTS FOR HIM",
+  ];
+  let currentIndex = 0;
+
+  if (searchButton && closeButton && searchPopUp) {
+    // Open the Search Pop-Up
+    searchButton.addEventListener("click", () => {
+      searchPopUp.setAttribute("aria-hidden", "false");
+    });
+
+    // Close the Search Pop-Up
+    closeButton.addEventListener("click", () => {
+      searchPopUp.setAttribute("aria-hidden", "true");
+    });
+
+    // Navigate Left or Right
+    navButtons.forEach((button, index) => {
+      button.addEventListener("click", () => {
+        currentIndex =
+          (currentIndex + (index === 0 ? -1 : 1) + options.length) %
+          options.length;
+        activeButton.textContent = options[currentIndex];
+      });
+    });
   }
 });
